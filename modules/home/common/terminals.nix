bab682a6423445d7ba6c474b26ec5d9b9b19cb16
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2024-12-18 23:23:20 +0100

    fzf: use catppuccin module for theming

--- modules/home/common/terminals.nix
+++ modules/home/common/terminals.nix
@@ -46,20 +46,7 @@ _: {
     fd.enable = true;
     fzf = {
       enable = true;
-      colors = {
-        "bg" = "#1e1e2e";
-        "bg+" = "#313244";
-        "fg" = "#cdd6f4";
-        "fg+" = "#cdd6f4";
-        "header" = "#f38ba8";
-        "hl" = "#f38ba8";
-        "hl+" = "#f38ba8";
-        "info" = "#cba6f7";
-        "marker" = "#f5e0dc";
-        "pointer" = "#f5e0dc";
-        "prompt" = "#cba6f7";
-        "spinner" = "#f5e0dc";
-      };
+      catppuccin.enable = true;
     };
     yazi = {
       enable = true;
