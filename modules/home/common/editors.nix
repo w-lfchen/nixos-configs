be19dea53941a2c7865703b910e5fb43a8eec947
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2025-02-28 19:42:19 +0100

    vscode: fix warnings
    i only have one profile so this should be fine

--- modules/home/common/editors.nix
+++ modules/home/common/editors.nix
@@ -29,27 +29,6 @@
   nixpkgs.overlays = [ inputs.nix-vscode-extensions.overlays.default ];
   programs.vscode = {
     enable = true;
-    enableUpdateCheck = false;
-    enableExtensionUpdateCheck = true;
-    extensions = with pkgs.vscode-marketplace; [
-      catppuccin.catppuccin-vsc
-      catppuccin.catppuccin-vsc-icons
-      dbaeumer.vscode-eslint
-      dsvictor94.promela
-      eww-yuck.yuck
-      esbenp.prettier-vscode
-      haskell.haskell
-      james-yu.latex-workshop
-      jnoortheen.nix-ide
-      justusadam.language-haskell
-      leonardssh.vscord
-      marp-team.marp-vscode
-      ms-vscode.hexeditor
-      myriad-dreamin.tinymist
-      rust-lang.rust-analyzer
-      tamasfe.even-better-toml
-      yoavbls.pretty-ts-errors
-    ];
     # this module should be removed imo, it's completely outdated and superseded
     # refer to general vscode config/extensions for haskell setup
     haskell.enable = false;
@@ -58,52 +37,75 @@
     # TODO: test whether this works correctly, if not, set to false
     mutableExtensionsDir = true;
     package = pkgs.vscodium;
-    # https://github.com/microsoft/vscode/issues/188624#issuecomment-1652888196
-    userSettings = {
-      "diffEditor.ignoreTrimWhitespace" = false;
-      "editor.fontFamily" =
-        "'FiraCode Nerd Font Mono', 'Material Symbols Rounded', 'monospace', monospace";
-      "editor.fontLigatures" = true;
-      "editor.renderWhitespace" = "all";
-      "hexeditor.columnWidth" = 16;
-      "hexeditor.defaultEndianness" = "little";
-      "hexeditor.inspectorType" = "aside";
-      "hexeditor.showDecodedText" = true;
-      "latex-workshop.latex.tools" = [
-        {
-          name = "latexmk";
-          command = "latexmk";
-          args = [
-            "--shell-escape"
-            "-synctex=1"
-            "-interaction=nonstopmode"
-            "-file-line-error"
-            "-lualatex"
-            #"-pdflatex"
-            "-outdir=%OUTDIR%"
-            "%DOC%"
-          ];
-        }
+    profiles.default = {
+      enableExtensionUpdateCheck = true;
+      enableUpdateCheck = false;
+      extensions = with pkgs.vscode-marketplace; [
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+        dbaeumer.vscode-eslint
+        dsvictor94.promela
+        eww-yuck.yuck
+        esbenp.prettier-vscode
+        haskell.haskell
+        james-yu.latex-workshop
+        jnoortheen.nix-ide
+        justusadam.language-haskell
+        leonardssh.vscord
+        marp-team.marp-vscode
+        ms-vscode.hexeditor
+        myriad-dreamin.tinymist
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
+        yoavbls.pretty-ts-errors
       ];
-      "latex-workshop.latexindent.args" = [
-        "-c"
-        "%DIR%/"
-        "%TMPFILE%"
-        "-l"
-        "-y=defaultIndent: '%INDENT%'"
-      ];
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings".nil.formatting.command = [ "nixfmt" ];
-      "tinymist.formatterMode" = "typstyle";
-      "window.titleBarStyle" = "custom";
-      "workbench.colorTheme" = "Catppuccin Mocha";
-      "workbench.iconTheme" = "catppuccin-mocha";
+      # https://github.com/microsoft/vscode/issues/188624#issuecomment-1652888196
+      userSettings = {
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "editor.fontFamily" =
+          "'FiraCode Nerd Font Mono', 'Material Symbols Rounded', 'monospace', monospace";
+        "editor.fontLigatures" = true;
+        "editor.renderWhitespace" = "all";
+        "hexeditor.columnWidth" = 16;
+        "hexeditor.defaultEndianness" = "little";
+        "hexeditor.inspectorType" = "aside";
+        "hexeditor.showDecodedText" = true;
+        "latex-workshop.latex.tools" = [
+          {
+            name = "latexmk";
+            command = "latexmk";
+            args = [
+              "--shell-escape"
+              "-synctex=1"
+              "-interaction=nonstopmode"
+              "-file-line-error"
+              "-lualatex"
+              #"-pdflatex"
+              "-outdir=%OUTDIR%"
+              "%DOC%"
+            ];
+          }
+        ];
+        "latex-workshop.latexindent.args" = [
+          "-c"
+          "%DIR%/"
+          "%TMPFILE%"
+          "-l"
+          "-y=defaultIndent: '%INDENT%'"
+        ];
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings".nil.formatting.command = [ "nixfmt" ];
+        "tinymist.formatterMode" = "typstyle";
+        "window.titleBarStyle" = "custom";
+        "workbench.colorTheme" = "Catppuccin Mocha";
+        "workbench.iconTheme" = "catppuccin-mocha";
 
-      # language settings
-      # tinymist (i think it's the culprit) kept wanting to add these
-      "[typst]"."editor.wordSeparators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
-      "[typst-code]"."editor.wordSeparators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
+        # language settings
+        # tinymist (i think it's the culprit) kept wanting to add these
+        "[typst]"."editor.wordSeparators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
+        "[typst-code]"."editor.wordSeparators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
+      };
     };
   };
 
