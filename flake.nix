0f0d16e1cf7ed789e36f5878af2baf5d0b0e7bc8
Wölfchen <w-lfchen@posteo.net>
2025-08-02 07:48:53 +0000

    flake.lock: update (#21)
    * flake.lock: update
    
    * lix: 2.93.2-1 -> main
    
    * hyprland: drop explicit sync options
    
    * refuge: remove unnecessary argument
    
    * vpn: add openconnect plugin

--- flake.nix
+++ flake.nix
@@ -24,10 +24,16 @@
       };
     };
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs = {
         flake-utils.follows = "flake-utils";
+        lix.follows = "lix";
         nixpkgs.follows = "nixpkgs";
       };
     };
@@ -72,6 +78,7 @@
       home-manager,
       catppuccin,
       eww,
+      lix,
       lix-module,
       nix-vscode-extensions,
       scripts-flake,
