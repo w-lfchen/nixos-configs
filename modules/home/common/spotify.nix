73fb0d53f910123bea345cbd33c4f6ca2367355c
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2025-11-24 20:30:26 +0100

    spotify: fix warning

--- modules/home/common/spotify.nix
+++ modules/home/common/spotify.nix
@@ -5,7 +5,7 @@
   unfree.allowedPackages = [ "spotify" ];
   programs.spicetify =
     let
-      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
     in
     {
       enable = true;
