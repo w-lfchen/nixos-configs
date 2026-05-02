69a53156e8f9f75620567b2353fd9c32cd03e303
Wölfchen <w-lfchen@posteo.net>
2026-05-02 13:38:52 +0200

    grub: improve theme override

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -21,17 +21,13 @@
   };
 
   catppuccin = {
-    sources.grub = inputs.catppuccin.packages.${pkgs.stdenv.hostPlatform.system}.buildCatppuccinPort {
-      port = "grub";
-      src = inputs.catppuccin-grub;
-      # source: https://github.com/catppuccin/nix/blob/deb2a5a54cf9e05ddf60aeeb933f60ad2fac20e1/pkgs/grub/package.nix
-      dontCatppuccinBuild = true;
-      dontCatppuccinInstall = true;
-      postInstall = ''
-        mkdir -p $out/share/grub
-        mv src $out/share/grub/themes
-      '';
-    };
+    sources = inputs.catppuccin.packages.${pkgs.stdenv.hostPlatform.system}.overrideScope (
+      final: prev: {
+        grub = prev.grub.overrideAttrs {
+          src = inputs.catppuccin-grub;
+        };
+      }
+    );
     grub.enable = true;
   };
 }
