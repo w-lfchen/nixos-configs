1a6b160c22be52fa4e4e879f8ac81d219068a80d
Wölfchen <w-lfchen@posteo.net>
2026-05-24 16:43:00 +0200

    wlogout: drop
    unused

--- hosts/voyage/home.nix
+++ hosts/voyage/home.nix
@@ -3,8 +3,6 @@
   home.stateVersion = "24.11"; # don't change this value
 
   # host specific config
-  programs.wlogout.enable = true;
-
   xdg.configFile."niri/config.kdl".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-configs/config/niri/voyage.kdl";
 }
--- modules/home/wm.nix
+++ modules/home/wm.nix
@@ -1,89 +1,35 @@
 { config, ... }:
 {
   # bars and launchers
-  programs = {
-    fuzzel = {
-      enable = true;
-      settings = {
-        border = {
-          radius = 0;
-          width = 2;
-          selection-radius = 0;
-        };
-        # can't use catppuccin.fuzzel since we want to adjust some colors
-        colors = {
-          background = "1e1e2eff";
-          text = "cdd6f4ff";
-          prompt = "bac2deff";
-          placeholder = "7f849cff";
-          input = "cdd6f4ff";
-          match = "cba6f7ff";
-          selection = "585b70ff";
-          selection-text = "cdd6f4ff";
-          selection-match = "cba6f7ff";
-          counter = "7f849cff";
-          border = "cba6f7ff";
-        };
-        main = {
-          dpi-aware = false;
-          font = "FiraCode Nerd Font Propo:size=14";
-          icons-enabled = false;
-          lines = 5;
-          width = 40;
-        };
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      border = {
+        radius = 0;
+        width = 2;
+        selection-radius = 0;
       };
-    };
-    # not really happy with this but it's enough for now.
-    # maybe use rofi or eww instead
-    wlogout = {
-      layout = [
-        {
-          action = "systemctl poweroff";
-          keybind = "h";
-          label = "shutdown";
-          text = "";
-        }
-        {
-          action = "systemctl reboot";
-          keybind = "r";
-          label = "reboot";
-          text = "";
-        }
-        {
-          action = "systemctl suspend";
-          keybind = "u";
-          label = "suspend";
-          text = "";
-        }
-      ];
-      style = ''
-        * {
-          background-image: none;
-          box-shadow: none;
-          font-size: 100px;
-          font-family: FiraCode Nerd Font Propo;
-        }
-
-        window {
-          background-color: rgba(17, 17, 27, 0.8);
-        }
-
-        button {
-          background-color: #313244;
-          margin: 50px;
-          margin-left: 600px;
-          margin-right: 600px;
-          text-decoration-color: #cdd6f4;
-          color: #cdd6f4;
-          background-repeat: no-repeat;
-          background-position: center;
-          background-size: 25%;
-        }
-
-        button:focus, button:active {
-          background-color: #45475a;
-        }
-      '';
+      # can't use catppuccin.fuzzel since we want to adjust some colors
+      colors = {
+        background = "1e1e2eff";
+        text = "cdd6f4ff";
+        prompt = "bac2deff";
+        placeholder = "7f849cff";
+        input = "cdd6f4ff";
+        match = "cba6f7ff";
+        selection = "585b70ff";
+        selection-text = "cdd6f4ff";
+        selection-match = "cba6f7ff";
+        counter = "7f849cff";
+        border = "cba6f7ff";
+      };
+      main = {
+        dpi-aware = false;
+        font = "FiraCode Nerd Font Propo:size=14";
+        icons-enabled = false;
+        lines = 5;
+        width = 40;
+      };
     };
   };
   # link eww configs into config dir
