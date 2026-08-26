280da6c1cfb0748220dbc1094b5214743b3c0a09
Wölfchen <w-lfchen@posteo.net>
2026-08-26 15:52:00 +0200

    idea: fix jdk override
    since nixos/nixpkgs@eadd6f50ffdfa3dca52328e43068ec13d63fdfa3, the jdk is
    no longer directly exposed

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -4,7 +4,7 @@
     discord
     element-desktop
     gimp
-    (jetbrains.idea.override { jdk = jetbrains.jdk-21; })
+    jetbrains.idea
     librewolf
     obsidian
     signal-desktop
@@ -14,6 +14,14 @@
     xournalpp
     zed-editor
     zotero
+  ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      jetbrains = prev.jetbrains // {
+        jdk = final.jetbrains.jdk-no-jcef-21;
+      };
+    })
   ];
 
   unfree.allowedPackages = [
